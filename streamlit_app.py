064c3ec91d5340a99d669a7659336e5652f407a9
Rosy-Elsa <110471741+Rosy-Elsa@users.noreply.github.com>
2022-08-10 22:35:29 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,5 +21,5 @@ color_list = df[0].values.tolist()
 
 # Let's put a pick list here so they can pick the color
 #option = streamlit.selectbox('Pick a sweatsuit color or style:', list(color_list))
-streamlit.list(color_list)
+list(color_list)
 
