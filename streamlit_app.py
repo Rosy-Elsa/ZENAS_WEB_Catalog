54927b7821b512002b1e4be44c518249f0f67e2d
Rosy-Elsa <110471741+Rosy-Elsa@users.noreply.github.com>
2022-08-10 22:15:29 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,13 +1,16 @@
 import snowflake.connector
 import streamlit
-import dataframes as df
+import pandas
 
-#my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-#my_cur = my_cnx.cursor()
-#my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(),CURRENT_REGION()")
-#my_data_row = my_cur.fetchone()
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("Select COLOR_OR_STYLE,SIZES_AVAILABLE from Catalog")
+my_data_row = my_cur.fetchall()
+
 streamlit.header("Zena's Amazing Athleisure Catalog")
 streamlit.text('Pick a sweatsuit color or style:')
 
-streamlit.write(df)
+
+streamlit.write(my_data_row)
+
 
